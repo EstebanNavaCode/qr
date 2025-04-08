11ef1ae67eda36a5ae973f4e263ace37a519c440
Esteban Nava <esteban@prointernet.mx>
2025-04-08 12:16:22 -0600

    abrir camara iOS y android
    Se abre la camara trasera en ambos sistemas y se eescanea el codigo qr, aun no hacen el intercambio de informacion

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,28 +1,30 @@
 import { getConnection, sql } from "../../sis/database/conection.js";
 
 export const loginUser = async (req, res) => {
-    const { username, password } = req.body;
-  
-    try {
-      const pool = await getConnection();
-      const result = await pool.request()
-        .input('email', sql.VarChar, username)
-        .input('password', sql.VarChar, password)
-        .query(`
-            SELECT id, nombre, apellido, telefono, correo, contrasena,empresa 
+  const { username, password } = req.body;
+
+  try {
+    const pool = await getConnection();
+    const result = await pool
+      .request()
+      .input("email", sql.VarChar, username)
+      .input("password", sql.VarChar, password).query(`
+            SELECT id, nombre, apellido, correo, contrasena
             FROM usuario 
             WHERE correo = @email AND contrasena = @password
           `);
-        
-      if (result.recordset.length > 0) {
-        const user = result.recordset[0];
-        res.json({ success: true, user });
-      } else {
-        res.json({ success: false, message: 'Correo o contraseña incorrectos ❌' });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ success: false, message: 'Error en el servidor' });
+
+    if (result.recordset.length > 0) {
+      const user = result.recordset[0];
+      res.json({ success: true, user });
+    } else {
+      res.json({
+        success: false,
+        message: "Correo o contraseña incorrectos ❌",
+      });
     }
-  };
-  
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Error en el servidor" });
+  }
+};
