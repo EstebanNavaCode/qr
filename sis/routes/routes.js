5486bc92c140abe17b7f883c687dc83a5bfebe3c
Esteban Nava <esteban@prointernet.mx>
2025-04-08 13:57:03 -0600

    MAIN

--- sis/routes/routes.js
+++ sis/routes/routes.js
@@ -1,10 +1,11 @@
 import  { Router } from "express";
 import { loginUser } from "../../src/controllers/login.controller.js";
-import { registrarIntercambio } from "../../src/controllers/intercambio.controller.js";
+import { registrarIntercambio, obtenerContactos } from "../../src/controllers/intercambio.controller.js";
 
 const router = Router();
 
 router.post("/login", loginUser);
 router.post("/registrar-intercambio", registrarIntercambio);
+router.get("/mis-contactos/:usuario_id", obtenerContactos);
 
 export default router;
--- src/controllers/intercambio.controller.js
+++ src/controllers/intercambio.controller.js
@@ -24,3 +24,30 @@ export const registrarIntercambio = async (req, res) => {
     res.status(500).json({ success: false, message: "Error en el servidor" });
   }
 };
+
+export const obtenerContactos = async (req, res) => {
+    const { usuario_id } = req.params;
+  
+    try {
+      const pool = await getConnection();
+      const result = await pool.request()
+        .input("usuario_id", sql.Int, usuario_id)
+        .query(`
+          SELECT 
+            u.id, u.nombre, u.apellido, u.empresa, u.correo, u.telefono
+          FROM intercambios i
+          JOIN usuario u ON u.id = 
+            CASE 
+              WHEN i.usuario_remitente_id = @usuario_id THEN i.usuario_destinatario_id
+              ELSE i.usuario_remitente_id
+            END
+          WHERE i.usuario_remitente_id = @usuario_id OR i.usuario_destinatario_id = @usuario_id
+        `);
+  
+      res.json(result.recordset);
+    } catch (error) {
+      console.error("Error al obtener contactos:", error);
+      res.status(500).json({ success: false, message: "Error al obtener contactos" });
+    }
+  };
+  
